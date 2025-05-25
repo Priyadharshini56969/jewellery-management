6a880c736c7c270a50080fc5ababf54edca82f33
Priyadharshini <b.priya1509@gmail.com>
2025-05-25 18:14:04 +0530

    Complete project upload with README

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
 $routes->get('/', 'Auth::login');
 $routes->get('login', 'Auth::login');
 $routes->post('login', 'Auth::doLogin');
